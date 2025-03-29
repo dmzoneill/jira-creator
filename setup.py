ca32a61626989a32b41289ba7d690b9430072981
Automated Publisher <actions@users.noreply.github.com>
2025-03-29 13:12:34 +0000

    Automated publish: Sat Mar 29 13:12:34 UTC 2025 81ecaa7503146a9c90913f2982ce613c03751941

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ entry_points = {
 
 setup_kwargs = {
     "name": "jira-creator",
-    "version": "0.0.15",
+    "version": "0.0.16",
     "description": "Jira console app",
     "long_description": "",
     "author": "David O Neill",

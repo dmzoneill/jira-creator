29728a3431b40fe9f48224d3b7f5043c5808f31c
David O Neill <dmz.oneill@gmail.com>
2025-04-24 14:21:47 +0100

    Refactor checks.sh to improve output messages and add new checks; update test_ai_helper_cli.py for import consistency.

--- checks.sh
+++ checks.sh
@@ -4,24 +4,32 @@ count=$(grep -r 'os.get' jira_creator/* | wc -l)
 
 # Check if the count is 1
 if [ "$count" -ne 1 ]; then
-    print "The value of os.get occurrences is > 1."
-    print "Are you using EnvFetcher?"
+    echo "The value of os.get occurrences is > 1."
+    echo "Are you using EnvFetcher?"
     exit 1
 fi
 
 count=$(grep -r "raise Exce" jira_creator/ | wc -l)
 
 # Check if the count is 1
-if [ "$count" -ne 1 ]; then
-    print "Raising bare exceptions is not allowed"
+if [ "$count" -gt 0 ]; then
+    echo "Raising bare exceptions is not allowed"
     exit 1
 fi
 
 count=$(grep -r "raise (" jira_creator/ | wc -l)
 
 # Check if the count is 1
-if [ "$count" -ne 1 ]; then
-    print "Superflous brackets"
+if [ "$count" -gt 0 ]; then
+    echo "Superflous brackets"
+    exit 1
+fi
+
+count=$(grep -r "# pylint" * | wc -l)
+
+# Check if the count is 1
+if [ "$count" -gt 23 ]; then
+    echo "Pylint creep"
     exit 1
 fi
 

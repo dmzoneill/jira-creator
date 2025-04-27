4c3960e555d96e7faaf870b6d213a7e5cd97767e
David O Neill <dmz.oneill@gmail.com>
2025-04-27 19:22:10 +0100

    Refactor CLI list issues command to utilize helper functions for improved readability and maintainability; update tests accordingly.

--- jira_creator/commands/cli_search.py
+++ jira_creator/commands/cli_search.py
@@ -28,8 +28,9 @@ from argparse import Namespace
 from typing import Any, List, Union
 
 from exceptions.exceptions import SearchError
-from jira_creator.core.view_helpers import format_and_print_rows, massage_issue_list
 from rest.client import JiraClient
+
+from jira_creator.core.view_helpers import format_and_print_rows, massage_issue_list
 
 
 # /* jscpd:ignore-start */
